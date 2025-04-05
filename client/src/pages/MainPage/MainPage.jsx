28f3c8945529396f7ba905711f780e694f3abf2c
Hann_zshish <seldmq@gmail.com>
2025-04-05 05:23:27 +0300

    rework main page

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import "./MainPage.css";
 import { useNavigate } from "react-router-dom";
+import mainPageBg from "../../images/mainPageBg.svg";
+import knifesCategory from "../../images/knifesCategory.svg";
+import gunsCategory from "../../images/gunsCategory.svg";
+import bulletsCategory from "../../images/bulletsCategory.svg";
+import clothesCategory from "../../images/clothesCategory.svg";
+import planesCategory from "../../images/planesCategory.svg";
+import tanksCategory from "../../images/tanksCategory.svg";
+import planeMain from "../../images/planeMain.svg";
 
 export default function MainPage() {
   const navigate = useNavigate();
@@ -9,8 +17,52 @@ export default function MainPage() {
   return (
     <div>
       <Header />
-      <div className="categories">
+      <div
+        className="categories"
+        style={{
+          backgroundImage: `url(${mainPageBg})`,
+          marginTop: "-1px",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "778px",
+          width: "1440px",
+        }}
+      >
         <div className="guns-knifes">
+          <img
+            onClick={() => navigate("/catalog/guns")}
+            className="guns-img"
+            src={gunsCategory}
+            alt=""
+          />
+          <img
+            onClick={() => navigate("/catalog/khifes")}
+            className="khifes-img"
+            src={knifesCategory}
+            alt=""
+          />
+        </div>
+        <div className="bullets-clothes">
+          <img
+            onClick={() => navigate("/catalog/bullets")}
+            src={bulletsCategory}
+            alt=""
+          />
+          <img
+            onClick={() => navigate("/catalog/ammunition")}
+            src={clothesCategory}
+            alt=""
+          />
+        </div>
+        <div className="planes-tanks">
+          <img src={planesCategory} alt="" />
+          <img src={tanksCategory} alt="" />
+        </div>
+        <div className="plane-main">
+          <img src={planeMain} alt="" />
+          <h1>NEW “F-14B TOMCAT</h1>
+        </div>
+        {/* <div className="guns-knifes">
           <div className="guns" onClick={() => navigate("/catalog/guns")}>
             <img src="https://telegraf.com.ua/static/storage/originals/4/5b/6e57e8e3f2526517125c13191ea3a5b4.jpg" />
             <label>Guns</label>
@@ -33,8 +85,7 @@ export default function MainPage() {
             <img src="https://us.tencatefabrics.com/hs-fs/hubfs/armyBackground.jpg?width=1920&name=armyBackground.jpg" />
             <label>Clothes</label>
           </div>
-        </div>
-        
+        </div> */}
       </div>
     </div>
   );
